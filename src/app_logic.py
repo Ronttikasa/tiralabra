f020454b072d7fc4769c40b9016a030b789e38c0
Silja <44512829+Ronttikasa@users.noreply.github.com>
2022-06-04 22:14:27 +0300

    Add application logic class

---
+++ src/app_logic.py
@@ -0,0 +1,18 @@
+from trie_service import TrieService
+from file_service import FileService
+
+class AppLogic:
+    def __init__(self, input_filename, output_filename, markov_degree):
+        self._input = input_filename
+        self._output = output_filename
+        self._markov_deg = markov_degree
+        self._file_svc = FileService()
+        self._trie_svc = TrieService()
+
+    def run(self):
+        raw_input_data = self._file_svc.read_file(self._input)
+        input_data = self._file_svc.create_sequences(raw_input_data)
+        self._trie_svc.insert(input_data)
+
+        generated_output = self._trie_svc.generate_sequence(self._markov_deg, 16)
+        self._file_svc.write_file(generated_output, self._output)
