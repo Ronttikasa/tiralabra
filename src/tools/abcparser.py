2b5ade4dbef1300eab3c48633b456a53807ed065
Silja <44512829+Ronttikasa@users.noreply.github.com>
2022-06-18 02:32:00 +0300

    Add output methods to abc parser

--- src/tools/abcparser.py
+++ src/tools/abcparser.py
@@ -1,8 +1,7 @@
 class ABCParser:
     def __init__(self):
         self._headers = ["X:", "T:", "C:", "M:", "K:", "R:", "L:"]
-        self._notes = "C,D,E,F,G,A,B,c'd'e'f'g'a'b'"
-        self._extra_characters = "(234"
+        self._notes = "CDEFGABcdefgab,'(234"
 
     def _remove_header(self, abc_data: list):
         """Remove the abd-file header data.
@@ -34,7 +33,7 @@ class ABCParser:
         output = []
         for row in sequence:
             for char in row:
-                if char in self._notes or char in self._extra_characters:
+                if char in self._notes:
                     output.append(char)
         return output
 
@@ -51,9 +50,7 @@ class ABCParser:
         i = 0
         while i < len(sequence):
             if sequence[i] == "(":
-                output.append(sequence[i+2])
-                output.append(sequence[i+3])
-                i += 5
+                i += 2
             elif i < len(sequence)-1 and sequence[i+1] in "234":
                 time_value = int(sequence[i+1])
                 for times in range(time_value):
@@ -66,7 +63,41 @@ class ABCParser:
                 output.append(sequence[i])
                 i += 1
         return output
-        
+
+    def prep_abc_for_trie(self, input_data: list):
+        data_v1 = self._remove_header(input_data)
+        data_v2 = self._strip_extra(data_v1)
+        data_v3 = self._modify_time_values(data_v2)
+        return data_v3
+
+    def _add_barlines(self, input_data: list):
+        output = ""
+        i = 0
+        while i < len(input_data):
+            output += input_data[i]
+            i += 1
+            if i % 8 == 0:
+                output += "|"
+            elif i % 4 == 0:
+                output += " "
+        return output
+
+    def _generate_header(self, title: str, key: str):
+        header = [
+            "X: 1",
+            "T: " + title,
+            "M: 4/4", 
+            "L: 1/8",
+            "K: " + key 
+        ]
+        return header
+
+    def convert_to_abc(self, notes_data: list, title: str, key: str):
+        header = self._generate_header(title, key)
+        abc = self._add_barlines(notes_data)
+        header.append(abc)
+        return header
+
 if __name__ == "__main__":
 
     input_data = [
@@ -79,7 +110,9 @@ if __name__ == "__main__":
         # "E|FAAB AFED|FAAB (3AAAde|fBBA Bcde|fdgf efdB|",
         # "FAAB AFED|FAAB A2de|fBBA BcdB|AFEF D3:|",
         # "e|faag fdde|fdad fdd2|efga beef|gebe gee2|",
-        "f'aaf (3baf|defd e2de|fB,BA BcdB|AFEF D3:|"
+        # "f'aaf faaf|defd e2de|fB,BA BcdB|AFEF D3:|"
+        "|:FAAB AFED|FA(3AAA ABde|fBBA Bcde|fBBA BcdA|",
+        "FAAB AFED|FAAB ABde|fBBA BcdB|AFEF D4:|"
     ]
 
     parser = ABCParser()
@@ -91,6 +124,11 @@ if __name__ == "__main__":
     print(cleaned_data)
     final_data = parser._modify_time_values(cleaned_data)
     print(final_data)
+
+    print("back to abc")
+
+    abc = parser.convert_to_abc(final_data, "testibiisi", "Dmaj")
+    print(abc)
 
     
 
