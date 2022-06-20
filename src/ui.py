05da55c198f7bce0aee3c7c838f866dc91288c20
Silja <44512829+Ronttikasa@users.noreply.github.com>
2022-06-20 03:45:32 +0300

    Rework ui

--- src/ui.py
+++ src/ui.py
@@ -3,17 +3,34 @@ from app_logic import AppLogic
 
 class UI:
     def __init__(self):
-        self._app = None
+        self._app = AppLogic()
 
-    def run(self):
-        if True:
-            input_file = input("Input file name: ")
-            markov_degree = int(input("Markov chain degree: "))
-            output_file = input("Output file name: ")
-        else:
-            input_file = "test_data.txt"
-            markov_degree = 2
-            output_file = "test_output.txt"
+    def ui(self):
+        print("Reel Generator")
+        print("")
+        print("This app generates reels (a Scottish/Irish dance tune) using Markov chains.")
+        print("The app uses abc notation format. Your teaching data file should be located in /data/input")
+        while True:
+            self._help()
+            command = input("Command: ")
+            if command == "q":
+                break
+            if command == "r":
+                input_filename = input("Input filename: ")
+                markov_degree = int(
+                    input("Highest Markov degree you want to use: "))
+                self._app.teaching_data_to_trie(
+                    input_filename, markov_degree + 1)
+                print("Generate a new tune")
+                tune_name = input("Name your tune: ")
+                bars = int(input("Number of bars to generate: "))
+                while markov_degree > 0:
+                    self._app.generate_and_save(markov_degree, bars, tune_name)
+                    markov_degree -= 1
+            else:
+                print("Command not found, try again")
 
-        self._app = AppLogic(input_file, output_file, markov_degree)
-        self._app.run()
+    def _help(self):
+        print("Commands:\n\
+            r: new teaching file\n\
+            q: quit")
